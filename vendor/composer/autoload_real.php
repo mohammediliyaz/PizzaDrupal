8f836bf7bcde7a7ebc72d361382ebf89db9f6088
mohammediliyaz <mohammedelyas14@gmail.com>
2021-06-23 11:02:47 +0530

    updated modules

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitfd20abbd69adbef166c8f206fc792f87
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitfd20abbd69adbef166c8f206fc792f87', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitfd20abbd69adbef166c8f206fc792f87', 'loadClassLoader'));
@@ -32,7 +34,7 @@ class ComposerAutoloaderInitfd20abbd69adbef166c8f206fc792f87
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitfd20abbd69adbef166c8f206fc792f87::getInitializer($loader));
         } else {
